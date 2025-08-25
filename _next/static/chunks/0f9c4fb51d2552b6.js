967602671ce9f267c6605a0eb2746ac1e89cea1e
Adeel Ali Yousaf <adeelaliyousaf01@gmail.com>
2025-08-26 01:33:00 +0500

    Updates

---
+++ _next/static/chunks/0f9c4fb51d2552b6.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/99dab2a548e00f33.js",
+  "static/chunks/turbopack-1d8ddef94b336a85.js"
+])
---
+++ _next/static/chunks/c1fc9306a8e0606f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/99dab2a548e00f33.js",
+  "static/chunks/turbopack-925d77464d604689.js"
+])
